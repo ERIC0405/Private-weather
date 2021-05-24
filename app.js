3be459c14a60359cf0caa23ecf47aeefe0777319
ERIC0405 <zhikailiang@student.mitt.ca>
2021-05-24 11:38:32 -0500

    render current weather card

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let apiKey = `b8aa8c39c4012f71e59cf320be595b6b`;
+const apiKey = `b8aa8c39c4012f71e59cf320be595b6b`;
 
 //Current Weather Data  
 //api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}
@@ -13,20 +13,32 @@ function success(position) {
   latitude = position.coords.latitude;
   longitude = position.coords.longitude;
   getCurrentWeatherJson(latitude, longitude)
+  .then((json) => renderCurrentWeather(json));
 }
 
 function error() {
-  status.textContent = 'Unable to retrieve your location';
+  status.textContent = "Unable to retrieve your location";
 }
 
 function getCurrentWeatherJson(latitude, longitude) {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`)
+  return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`)
     .then((response) => {
       if (response.ok) {
         return response.json();
       } else {
         throw new Error(`There is a problem in current weather`);
       }
-    })
-    .then((json) => console.log(json));
-} 
+    });
+  }
+  
+  function renderCurrentWeather(json) {
+    const currentConditionsEle = document.querySelector(`.current-conditions`);
+  
+    currentConditionsEle.innerHTML = `
+       <h2>Current Conditions</h2>
+       <img src="http://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png" />
+       <div class="current">
+       <div class="temp">${Math.round(json.main.temp)}℃</div>
+       <div class="condition">${json.weather[0].description}</div>
+      `;
+  }
